f73862051cd3d9a88b273f1b059ee76df92d224a
Amanda <amandamaarman19911@gmail.com>
2021-05-04 16:42:12 +0200

    revamped portfolio

--- js/menu.js
+++ js/menu.js
@@ -1,120 +1,120 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     homeElem = document.getElementById('home');
-    b_control = document.getElementById('batch_control');
-    b_declaration = document.getElementById('batch_declaration');
-    b_download = document.getElementById('batch_download');
-    b_treatment = document.getElementById('batch_treatment');
+    fed_projElem = document.getElementById('fed_proj');
+    bed_projElem = document.getElementById('bed_proj');
+    aboutMeElem = document.getElementById('aboutMe');
+    serviceElem = document.getElementById('service');
 
     homeElem_content = document.getElementById('home_content');
-    b_control_content = document.getElementById('batch_control_content');
-    b_declaration_content = document.getElementById('batch_declaration_content');
-    b_download_content = document.getElementById('batch_download_content');
-    b_treatment_content = document.getElementById('batch_treatment_content');
+    fed_proj_contentElem = document.getElementById('fed_proj_content');
+    bed_proj_contentElem = document.getElementById('bed_proj_content');
+    aboutMe_contentElem = document.getElementById('aboutMe_content');
+    service_contentElem = document.getElementById('service_content');
 
-    home.addEventListener('click', function() {
-        /* État actif de l'onglet */
+    homeElem.addEventListener('click', function() {
+        /* home link */
 
-        if (b_control.classList.contains('active') || b_declaration.classList.contains('active') || b_download.classList.contains('active') || b_treatment.classList.contains('active')) {
-            b_control.classList.remove('active');
-            b_declaration.classList.remove('active');
-            b_download.classList.remove('active');
-            b_treatment.classList.remove('active');
+        if (fed_projElem.classList.contains('active') || bed_projElem.classList.contains('active') || aboutMeElem.classList.contains('active') || serviceElem.classList.contains('active')) {
+            fed_projElem.classList.remove('active');
+            bed_projElem.classList.remove('active');
+            aboutMeElem.classList.remove('active');
+            serviceElem.classList.remove('active');
             homeElem.classList.add('active');
         } else {
             homeElem.classList.add('active');
         }
 
-        /* Affichage en fonction de l'id cliqué */
+        /* home content */
         homeElem_content.classList.remove('none');
-        b_control_content.classList.add('none');  
-        b_declaration_content.classList.add('none');  
-        b_download_content.classList.add('none');  
-        b_treatment_content.classList.add('none');  
+        fed_proj_contentElem.classList.add('none');  
+        bed_proj_contentElem.classList.add('none');  
+        aboutMe_contentElem.classList.add('none');  
+        service_contentElem.classList.add('none');  
     });
 
-    b_control.addEventListener('click', function() {
-        /* État actif de l'onglet */
+    fed_projElem.addEventListener('click', function() {
+        /* Fed Link */
 
-        if (homeElem.classList.contains('active') || b_declaration.classList.contains('active') || b_download.classList.contains('active') || b_treatment.classList.contains('active')) {
+        if (homeElem.classList.contains('active') || bed_projElem.classList.contains('active') || aboutMeElem.classList.contains('active') || serviceElem.classList.contains('active')) {
             homeElem.classList.remove('active');
-            b_declaration.classList.remove('active');
-            b_download.classList.remove('active');
-            b_treatment.classList.remove('active');
-            b_control.classList.add('active');
+            bed_projElem.classList.remove('active');
+            aboutMeElem.classList.remove('active');
+            serviceElem.classList.remove('active');
+            fed_projElem.classList.add('active');
         } else {
-            b_control.classList.add('active');
+            fed_projElem.classList.add('active');
         }
 
-        /* Affichage en fonction de l'id cliqué */
-        b_control_content.classList.remove('none');  
+        /* fed content */
+        fed_proj_contentElem.classList.remove('none');  
         homeElem_content.classList.add('none');  
-        b_declaration_content.classList.add('none');  
-        b_download_content.classList.add('none');  
-        b_treatment_content.classList.add('none');  
+        bed_proj_contentElem.classList.add('none');  
+        aboutMe_contentElem.classList.add('none');  
+        service_contentElem.classList.add('none');  
     });
 
-    b_declaration.addEventListener('click', function() {
+    bed_projElem.addEventListener('click', function() {
         /* État actif de l'onglet */
 
-        if (homeElem.classList.contains('active') || b_control.classList.contains('active') || b_download.classList.contains('active') || b_treatment.classList.contains('active')) {
+        if (homeElem.classList.contains('active') || fed_projElem.classList.contains('active') || aboutMeElem.classList.contains('active') || serviceElem.classList.contains('active')) {
             homeElem.classList.remove('active');
-            b_control.classList.remove('active');
-            b_download.classList.remove('active');
-            b_treatment.classList.remove('active');
-            b_declaration.classList.add('active');
+            fed_projElem.classList.remove('active');
+            aboutMeElem.classList.remove('active');
+            serviceElem.classList.remove('active');
+            bed_projElem.classList.add('active');
         } else {
-            b_declaration.classList.add('active');
+            bed_projElem.classList.add('active');
         }
 
         /* Affichage en fonction de l'id cliqué */
         homeElem_content.classList.add('none');
-        b_control_content.classList.add('none');    
-        b_download_content.classList.add('none');  
-        b_treatment_content.classList.add('none');
-        b_declaration_content.classList.remove('none');  
+        fed_proj_contentElem.classList.add('none');    
+        aboutMe_contentElem.classList.add('none');  
+        service_contentElem.classList.add('none');
+        bed_proj_contentElem.classList.remove('none');  
     });
 
-    b_download.addEventListener('click', function() {
+    aboutMeElem.addEventListener('click', function() {
         /* État actif de l'onglet */
 
-        if (homeElem.classList.contains('active') || b_control.classList.contains('active') || b_declaration.classList.contains('active') || b_treatment.classList.contains('active')) {
+        if (homeElem.classList.contains('active') || fed_projElem.classList.contains('active') || bed_projElem.classList.contains('active') || serviceElem.classList.contains('active')) {
             homeElem.classList.remove('active');
-            b_control.classList.remove('active');
-            b_declaration.classList.remove('active');
-            b_treatment.classList.remove('active');
-            b_download.classList.add('active');
+            fed_projElem.classList.remove('active');
+            bed_projElem.classList.remove('active');
+            serviceElem.classList.remove('active');
+            aboutMeElem.classList.add('active');
         } else {
-            b_download.classList.add('active');
+            aboutMeElem.classList.add('active');
         }
 
         /* Affichage en fonction de l'id cliqué */
         homeElem_content.classList.add('none');
-        b_control_content.classList.add('none');  
-        b_declaration_content.classList.add('none');    
-        b_treatment_content.classList.add('none');
-        b_download_content.classList.remove('none'); 
+        fed_proj_contentElem.classList.add('none');  
+        bed_proj_contentElem.classList.add('none');    
+        service_contentElem.classList.add('none');
+        aboutMe_contentElem.classList.remove('none'); 
     });
 
-    b_treatment.addEventListener('click', function() {
+    serviceElem.addEventListener('click', function() {
         /* État actif de l'onglet */
 
-        if (homeElem.classList.contains('active') || b_control.classList.contains('active') || b_declaration.classList.contains('active') || b_download.classList.contains('active')) {
+        if (homeElem.classList.contains('active') || fed_projElem.classList.contains('active') || bed_projElem.classList.contains('active') || aboutMeElem.classList.contains('active')) {
             homeElem.classList.remove('active');
-            b_control.classList.remove('active');
-            b_declaration.classList.remove('active');
-            b_download.classList.remove('active');
-            b_treatment.classList.add('active');
+            fed_projElem.classList.remove('active');
+            bed_projElem.classList.remove('active');
+            aboutMeElem.classList.remove('active');
+            serviceElem.classList.add('active');
         } else {
-            b_treatment.classList.add('active');
+            serviceElem.classList.add('active');
         }
 
         /* Affichage en fonction de l'id cliqué */
         homeElem_content.classList.add('none');
-        b_control_content.classList.add('none');  
-        b_declaration_content.classList.add('none');  
-        b_download_content.classList.add('none');  
-        b_treatment_content.classList.remove('none'); 
+        fed_proj_contentElem.classList.add('none');  
+        bed_proj_contentElem.classList.add('none');  
+        aboutMe_contentElem.classList.add('none');  
+        service_contentElem.classList.remove('none'); 
     });
 
 });
